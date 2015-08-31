c722da70fe4de36a6f738db6155e7bc39bc30632
Jason & Leah <Jason+Leah@appacademy.io>
2015-08-31 11:31:23 -0400

    Finish arguments

--- arguments.js
+++ arguments.js
@@ -2,7 +2,7 @@ function sum() {
   var sum = 0;
   var args = Array.prototype.slice.apply(arguments);
   for (var i = 0; i < args.length; i++) {
-    sum += args[i];
+    sum += parseInt(args[i]);
   }
   return sum;
 };
@@ -15,21 +15,49 @@ Function.prototype.myBind = function (obj) {
   };
 };
 
-function Cat(name) {
-  this.name = name;
-};
-
-Cat.prototype.sayHi = function() {
-  return "Hello, " + this.name;
-}
-Cat.prototype.count = function(start,end) {
-  for (var i = start; i <= end; i++) {
-    console.log("I "+ this.name + "say " + i);
+var otherSum = function (arr) {
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += parseInt(arr[i]);
   }
+  return sum;
 };
 
-var rocky = new Cat("Rocky");
-var sayHiFunction = rocky.sayHi.myBind(rocky);
-var catCountToNine = rocky.count.myBind(rocky, 1, 9);
 
-catCountToNine();
+function curriedSum(numArgs) {
+  var numbers = [];
+  var _curriedSum = function(num) {
+    numbers.push(num);
+    if (numbers.length === numArgs) {
+      return otherSum(numbers);
+    }
+    else {
+      return _curriedSum;
+    }
+  }
+  return _curriedSum;
+}
+
+
+
+// ***for testing purposes****
+// var manySum = curriedSum(2);
+// manySum(5)(30);
+// function Cat(name) {
+//   this.name = name;
+// };
+//
+// Cat.prototype.sayHi = function() {
+//   return "Hello, " + this.name;
+// }
+// Cat.prototype.count = function(start,end) {
+//   for (var i = start; i <= end; i++) {
+//     console.log("I "+ this.name + "say " + i);
+//   }
+// };
+//
+// // var rocky = new Cat("Rocky");
+// // var sayHiFunction = rocky.sayHi.myBind(rocky);
+// // var catCountToNine = rocky.count.myBind(rocky, 1, 9);
+// //
+// // catCountToNine();
